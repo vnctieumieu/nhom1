0ccb91be778002152ba480e266a2d1ec961df6f3
Nguyen Phuoc <vcntieumieu@gmail.com>
2020-10-07 15:19:50 +0700

    Phan Cong

---
+++ source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b);
+int hieu(int a, int b);
+int tich(int a, int b);
+float thuong(int a, int b);
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap 2 so nguyen: ";
+	cin >> a >> b;
+	cout << "Tinh gi? (+,-,*,/) ";
+	cin >> ch;
+	switch (phepToan)
+	{
+	case '+':
+		
+		break;
+	case '-':
+
+		break;
+	case '*':
+
+		break;
+	case '/':
+
+		break;
+	default:
+		cout << "Chon sai phep toan";
+		break;
+	}
+	return 0;
+}
