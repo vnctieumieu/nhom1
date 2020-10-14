5ca6f27e90829ed4be6367b0228ca9a8ec22fe9a
Nguyen Quoc Trung <quachquesh2@gmail.com>
2020-10-14 16:10:04 +0700

    update

--- source.cpp
+++ source.cpp
@@ -5,44 +5,6 @@ int tong(int a, int b){
 	kq = a + b;
 	return kq;
 }
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
-float chuvihcn(int dai, int rong);
-int luyenTapPhepCong();
-int main()
-{
-	int a, b;
-	char phepToan;
-	cout << "Nhap 2 so nguyen: ";
-	cin >> a >> b;
-	cout << "Tinh gi? (+,-,*,/) ";
-	cin >> ch;
-	switch (phepToan)
-	{
-	case '+':
-		cout << "Tong " << a << " & " << b <<" = " << tong(a,b);
-		break;
-	case '-':
-		cout << a << "-" << b << "= " << hieu(a,b) << endl;
-		break;
-	case '*':
-		cout << a << " * " << b << " = " << tich(a,b) << endl;
-		break;
-	case '/':
-		cout<<a<<" / "<<b<<" = "<< thuong(a,b)<<endl;
-		break;
-	case 'cvhv':
-		count << "chu vi hinh vuong: " << chuViHinhVuong(a) << endl;
-	case 'ltpc':
-		luyenTapPhepCong();
-	default:
-		cout << "Chon sai phep toan";
-		break;
-	}
-	return 0;
-}
-
 int hieu(int a, int b){
 	return a-b;
 }
@@ -54,16 +16,20 @@ int tich(int a, int b) {
 float thuong(int a, int b){
 	return a/b;
 }
-
-
+int dientichhcn(int a, int b){
+	return a*b;
+}
+int dientichhv(int a){
+	return pow(a,2);
+}
+int dientichht(int r){
+	return 3,14 * pow(r,2);  // dien Pi * (ban kinh) ^ 2
+}
 float chuvihcn(int dai, int rong)
-{
-		cout<<"nhap chieu dai:"<<endl;
-		cin>>dai;
-		cout<<"nhap chieu rong:"<<endl;
-		cin>>rong;
-		cout<<"chu vi hinh chu nhat la"<<(dai+rong)*2;
-		
+{	
+	    int kq = ( dai + rong ) * 2;
+		cout<<"chu vi hinh chu nhat la"<< kq ; // lan sau kg dc nhap xuat tham so truyen trong ham va khong dc cout trong ham.
+		return kq;
 }
 
 float chuViHinhTron(int a){
@@ -86,4 +52,57 @@ void luyenTapPhepCong() {
 	} else {
 		cout << "Dap an sai";
 	}
+}
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap 2 so nguyen: ";
+	cin >> a >> b;
+	int r;
+	cout <<"Nhap ban kinh hinh tron" << endl;
+	cout << "A o tren dong thoi la chieu dai hnc va canh hv. B la chieu rong hcn" << endl;
+	cin >> r;
+	cout << "Tinh gi? (+,-,*,/,cn,v,tron,cvhcn,cvhv,cvht) ";
+	cin >> ch;
+	switch (phepToan)
+	{
+	case '+':
+		cout << "Tong " << a << " & " << b <<" = " << tong(a,b);
+		break;
+	case '-':
+		cout << a << "-" << b << "= " << hieu(a,b) << endl;
+		break;
+	case '*':
+		cout << a << " * " << b << " = " << tich(a,b) << endl;
+		break;
+	case '/':
+		cout<<a<<" / "<<b<<" = "<< thuong(a,b)<<endl;
+		break;
+	case 'cvhcn':
+		count << "chu vi hinh chu nhat: " << chuvihcn(a, b) << endl;
+		break;
+	case 'cvhv':
+		count << "chu vi hinh vuong: " << chuViHinhVuong(a) << endl;
+		break;
+	case 'cvht':
+		count << "chu vi hinh tron: " << chuViHinhTron(r) << endl;
+		break;
+	case 'cn': 
+		cout <<"Dien tich hinh cn la: " << dientichhcn(a,b) << " m^2 " << endl; 
+		break;
+	case 'v':
+		cout <<"Dien tich hinh vuong la: " << dientichhv(a) << " m^2 " << endl; 
+		break;
+	case 'tron':
+		cout <<"Dien tich hinh tron la: " << dientichhht(r) << " m^2 " << endl; 
+		break;
+	case 'ltpc'
+		luyenTapPhepCong();
+		break;
+	default:
+		cout << "Chon sai phep toan or sai hinh";
+		break;	
+	}
+	return 0;
 }
