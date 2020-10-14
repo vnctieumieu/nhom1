18815ade222d5b3775a08fdc8d8ec124f9203027
Nguyen Phuoc <vnctieumieu@gmail.com>
2020-10-14 15:24:22 +0700

    Update dien tich cac hinh

--- source.cpp
+++ source.cpp
@@ -8,6 +8,15 @@ int tong(int a, int b){
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int dientichhcn(int a, int b){
+	return a*b;
+}
+int dientichhv(int a){
+	return pow(a,2);
+}
+int dientichht(int r){
+	return 3,14 * pow(r,2);  // dien Pi * (ban kinh) ^ 2
+}
 int main()
 {
 	int a, b;
@@ -32,6 +41,28 @@ int main()
 		break;
 	default:
 		cout << "Chon sai phep toan";
+		break;
+	}
+	int r;
+	cout <<"Nhap ban kinh hinh tron" << endl;
+	cout << "A o tren dong thoi la chieu dai hnc va canh hv. B la chieu rong hcn" << endl;
+	cin >> r;
+	char hinh;
+	cout << "Hinh gi? ? (cn,v,tron) ";
+	cin >> hinh;
+	switch(hinh)
+	{
+		case 'cn': 
+		cout <<"Dien tich hinh cn la: " << dientichhcn(a,b) << " m^2 " << endl; 
+		break;
+		case 'v':
+		cout <<"Dien tich hinh vuong la: " << dientichhv(a) << " m^2 " << endl; 
+		break;
+		case 'tron':
+		cout <<"Dien tich hinh tron la: " << dientichhht(r) << " m^2 " << endl; 
+		break;
+		default:
+		cout << "Chon sai hinh";
 		break;
 	}
 	return 0;
